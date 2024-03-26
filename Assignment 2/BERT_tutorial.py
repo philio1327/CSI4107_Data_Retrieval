a3039f8023495f860aea28bc40216bdc11429026
philthethrill <pande021@uottawa.ca>
2024-03-25 23:27:57 -0400

    Added AP_collection and updated Python code
    ->text files split by DOC
    ->document_dict is a dictionary of all documents. Can be accessed using the DOCNO. Format {key=docno, value=(head, text)}

---
+++ Assignment 2/BERT_tutorial.py
@@ -0,0 +1,4 @@
+import tensorflow_hub as hub
+import tensorflow_text as text
+
+encoder_url = "https://www.kaggle.com/models/google/bert/frameworks/tensorFlow1/variations/bert-uncased-l-12-h-768-a-12"
--- Assignment 2/assignment2_Group_15.py
+++ Assignment 2/assignment2_Group_15.py
@@ -13,6 +13,7 @@ import requests
 import re
 from nltk import PorterStemmer as porter
 import string
+import os
 
 ##########################################################################################################
 # PRE-PROCESSING FUNCTIONS FROM ASSIGNMENT 1
@@ -39,7 +40,7 @@ def get_words_from_webpage(url):
 def read_file(filename):
     try:
         start = time.time()
-        filepath = f"src\\AP_collection\\coll\\{filename}"
+        filepath = f"AP_collection\\coll\\{filename}"
         with open(filepath, "r") as file:
             content = file.read()
         end = time.time()
@@ -62,86 +63,71 @@ def remove_html_tags(content):
     clean_content = re.sub(r"<[^>]*>", "", content)
     return clean_content
 
-def process_text(text):
-    # Remove punctuation marks
-    text = remove_punctuation(text)
-    # Convert text to lowercase
-    text = text.lower()
-    # Tokenize the text into words
-    words = text.split()
-    return words
-
-def remove_punctuation(text):
-    clean_text = re.sub(rf"[{string.punctuation}]", " ", text)
-    return clean_text
-
-def remove_stopwords(list_of_words, stop_words_list):
-    return [word for word in list_of_words if word not in stop_words_list]
-
-def stem_words(content):
-  # Initialize PorterStemmer
-  stems = [porter.stem(word) for word in content]
-  return stems
-
-def get_tokens(doc_content, stop_words_list):
-    stop_words_list = set(stop_words_list)
-    # To read the file from the command line
-    # if len(sys.argv) != 2:
-    #     print("Usage: python Assignment_1_Group15.py <filename>")
-    #     print("Example: python Assignment_1_Group15.py AP880212")
-    #     filename1 = input("Enter Name of File Here: ")
-
-    #     file_content = read_file(filename1)
-    # else:
-    #     filename = sys.argv[1]
-    #     file_content = read_file(filename)
-    #file_content = read_file(filename)
-
-    read_file_end = time.time()
-    #print(f"Read File Time: {file_content[1]} Seconds")
-
-    clean_content = remove_html_tags(doc_content)
-    html_remove_end = time.time()
-    #print(f"Removed HTML tags Time: {html_remove_end-read_file_end} Seconds")
-
-    list_of_words = process_text(clean_content)
-    punct_remove_time = time.time()
-    #print(f"Removed Punctuation and Lowercase the words Time: {punct_remove_time-html_remove_end} Seconds")
-
-    words_without_stopwords = remove_stopwords(list_of_words, stop_words_list)
-    remove_stopwords_time = time.time()
-    #print(f"Remove Stopwords Time: {remove_stopwords_time-punct_remove_time} Seconds")
-    #print(words_without_stopwords)
-    #print("Number of words: " + str(len(words_without_stopwords)))
-
-    # apply Porter stemming
-    stemmed_tokens = stem_words(words_without_stopwords)
-    stemming_time = time.time()
-    # print(f"Stem words Time: {stemming_time-remove_stopwords_time} Seconds")
-
-    return stemmed_tokens
-
-def get_docID_text(doc):
-  # getting id
-  docID = doc.split("</DOCNO>")[0]
-  docID = docID.split("<DOCNO>")[1].strip()
-
-  head_info = doc.split("<HEAD>")
-  all_head_info = ' '.join([x.split("</HEAD>")[0].strip() for x in head_info[1:]])
-
-  text_info = doc.split("<TEXT>")
-  all_text_info = ' '.join([x.split("</TEXT>")[0].strip() for x in text_info[1:]])
-
-  searchable = all_head_info + " " + all_text_info
-
-  clean_doc = remove_html_tags(searchable).strip()
-  clean_doc = clean_doc.replace("\n", " ")
-
-  if len(clean_doc) <= 1: # there is no text in doc
-    clean_doc = docID
-
-  return (docID, clean_doc)
 ##########################################################################################################
 # END CODE FROM ASSIGNMENT 1
 ##########################################################################################################
+def extract_docno_head_text(document):
+    docno_pattern = re.compile(r'<DOCNO>(.*?)</DOCNO>')
+    head_pattern = re.compile(r'<HEAD>(.*?)</HEAD>')
+
+    # Extract document number and headline
+    docno_match = docno_pattern.search(document)
+    head_match = head_pattern.search(document)
+    if docno_match and head_match:
+        docno = docno_match.group(1).strip()
+        head = head_match.group(1).strip()
+        # Extract Text content excluding <TEXT> tags
+        text_content = document.split('<TEXT>', 1)[-1].split('</TEXT>', 1)[0].strip()
+        return (docno, head, text_content)
+    return None
+def read_all_files_in_directory():
+    document_list = []
+    # Get the directory containing the current Python script
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Relative path to the folder containing your files
+    folder_path = os.path.join(script_directory, 'AP_collection', 'coll')
+
+    # Iterate over each file in the folder
+    for filename in os.listdir(folder_path):
+        # Check if the item is a file (not a directory)
+        if os.path.isfile(os.path.join(folder_path, filename)):
+            # Construct the full path to the file
+            file_path = os.path.join(folder_path, filename)
+            # Open the file and read its contents
+            file_content = read_file_direct(file_path)
+            # split content by <DOC> tag
+            split_content = file_content.split("<DOC>")
+            for doc in split_content:
+                result = extract_docno_head_text(doc)
+                if result:
+                    document_list.append(result)
+    return document_list
+
+def transform_to_dictionary(document_list):
+    document_dict = {}
+    for docno, head, text_content in document_list:
+        document_dict[docno] = (head, text_content)
+    return document_dict
+
+
+if __name__ == "__main__":
+    document_list = read_all_files_in_directory()
+    document_dict = transform_to_dictionary(document_list)
+
+    # You can get all the keys by typing document_dict.keys()
+    # if you type print(document_dict["AP880212-0001"]) you will get a tuple of the form (head, text)
+    # Sample print out:
+
+    # Print the document with new lines
+    docno = "AP880212-0001"
+    head, text_content = document_dict[docno]
+    print("Head:", head)
+    print("Text:")
+    # Split the text content by newline and print each line separately
+    for line in text_content.split("\n"):
+        print(line)
+
+
+
 
