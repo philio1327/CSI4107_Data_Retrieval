10ca28157ea9420641ba6ce9ff124c65675c0a0f
philio1327 <109395973+philio1327@users.noreply.github.com>
2024-03-26 20:35:11 -0400

    Verify prediction in Image Processing

--- Assignment 2/Image_Processing.py
+++ Assignment 2/Image_Processing.py
@@ -9,7 +9,7 @@ fashion_mnist = keras.datasets.fashion_mnist  # load dataset
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # split into tetsing and training
 # check the data
-train_images.shape
+print(train_images.shape)
 
 train_images[0,23,23]  # let's have a look at one pixel
 
@@ -59,3 +59,42 @@ np.argmax(predictions[0])
 
 # Check if correct
 test_labels[0]
+
+# Verifying Prediction
+COLOR = 'white'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR
+
+def predict(model, image, correct_label):
+  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+  prediction = model.predict(np.array([image]))
+  predicted_class = class_names[np.argmax(prediction)]
+
+  show_image(image, class_names[correct_label], predicted_class)
+
+
+def show_image(img, label, guess):
+  plt.figure()
+  plt.imshow(img, cmap=plt.cm.binary)
+  plt.title("Excpected: " + label)
+  plt.xlabel("Guess: " + guess)
+  plt.colorbar()
+  plt.grid(False)
+  plt.show()
+
+
+def get_number():
+  while True:
+    num = input("Pick a number: ")
+    if num.isdigit():
+      num = int(num)
+      if 0 <= num <= 1000:
+        return int(num)
+    else:
+      print("Try again...")
+
+num = get_number()
+image = test_images[num]
+label = test_labels[num]
+predict(model, image, label)
